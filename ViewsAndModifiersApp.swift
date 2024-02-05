8e80859c5298efda01715ab1311365f1fe838c25
LikoSetiawan <128354782+LikoSetiawan@users.noreply.github.com>
2024-02-05 22:33:20 +0800

    Initial commit

---
+++ ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Liko Setiawan on 05/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
